90c5fa9129c0bc1f310e6eebc08187c3fcb1f8e7
Yuen <yuenko0110@gmail.com>
2025-12-04 13:52:40 +0800

    viper config

--- main.go
+++ main.go
@@ -7,23 +7,22 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/proyuen/simple-bank/api"
 	db "github.com/proyuen/simple-bank/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgres://root:secret@localhost:5432/simplebank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/proyuen/simple-bank/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 	store := db.Newstore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.Server_Address)
 	if err != nil {
 		log.Fatal("cannot start server", err)
 	}
---
+++ util/config.go
@@ -0,0 +1,25 @@
+package util
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBDriver       string `mapstructure:"DBDRIVER"`
+	DBSource       string `mapstructure:"DBSOURCE"`
+	Server_Address string `mapstructure:"SERVER_ADDRESS"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+
+}
